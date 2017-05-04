c46850c937ceb95bf1f85ffd963bda879c7b9f5c
Javen <914301050@qq.com>
2017-05-04 19:21:18 +0800

    sessionid生成方法

--- config/default.js
+++ config/default.js
@@ -1,6 +1,7 @@
 const appConfig = require('../config.json');
 
 module.exports = {
+  version: '1.0.2',
   appId: appConfig.appId,
   appSecret: appConfig.appSecret
 };
--- util/utility.js
+++ util/utility.js
@@ -1,4 +1,7 @@
 const crypto = require('crypto');
+const moment = require('moment');
+const uuidV4 = require('uuid/v4');
+const config = require('config');
 
 function createSha1Signature(str) {
   const sha1 = crypto.createHash('sha1');
@@ -7,33 +10,40 @@ function createSha1Signature(str) {
 }
 
 function decryptData(appId, sessionKey, encryptedData, iv) {
-    // base64 decode
-    sessionKey = Buffer.from(sessionKey, 'base64');
-    encryptedData = Buffer.from(encryptedData, 'base64');
-    iv = Buffer.from(iv, 'base64');
+  // base64 decode
+  sessionKey = Buffer.from(sessionKey, 'base64');
+  encryptedData = Buffer.from(encryptedData, 'base64');
+  iv = Buffer.from(iv, 'base64');
 
-    let decoded;
-    try {
-      // 解密
-      const decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv);
-      // 设置自动 padding 为 true，删除填充补位
-      decipher.setAutoPadding(true);
+  let decoded;
+  try {
+    // 解密
+    const decipher = crypto.createDecipheriv('aes-128-cbc', sessionKey, iv);
+    // 设置自动 padding 为 true，删除填充补位
+    decipher.setAutoPadding(true);
 
-      decoded = decipher.update(encryptedData, 'binary', 'utf8');
-      decoded += decipher.final('utf8');
-      decoded = JSON.parse(decoded);
+    decoded = decipher.update(encryptedData, 'binary', 'utf8');
+    decoded += decipher.final('utf8');
+    decoded = JSON.parse(decoded);
 
-    } catch (err) {
-      throw new Error('Illegal Buffer');
-    }
+  } catch (err) {
+    throw new Error('Illegal Buffer');
+  }
 
-    if (decoded.watermark.appid !== appId) {
-      throw new Error('Illegal Buffer');
-    }
+  if (decoded.watermark.appid !== appId) {
+    throw new Error('Illegal Buffer');
+  }
 
-    return decoded;
+  return decoded;
+}
+
+function createSessionid(openid, sessionKey) {
+  const uuid = uuidV4();
+  const timestamp = moment().valueOf();
+  const version = config.version;
+  return '';
 }
 
 module.exports = {
-  createSha1Signature, decryptData
+  createSha1Signature, decryptData, createSessionid
 };
